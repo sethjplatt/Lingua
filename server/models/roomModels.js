cfdedc6175da5aff7dd4ceb5fb4f6f46ef37d2ce
seth <contact.sethjplatt@gmail.com>
2023-01-13 12:23:15 +0100

    starting translation work

--- server/models/roomModels.js
+++ server/models/roomModels.js
@@ -1,12 +1,26 @@
 const client = require('./index');
 const db = client.db('test').collection('rooms');
+const userDb = client.db('test').collection('users');
 
 async function joinRoom(roomData) {
-  // console.log('room data:', roomData);
   const dbResponse = await db.findOne({ roomId: roomData.roomId });
   console.log('dbResponse:', dbResponse);
   if (!dbResponse) {
-    const created = await db.insertOne({ ...roomData, messages: [] });
+    console.log('room data:', roomData);
+    const roomCreater = await userDb.findOne({
+      userName: roomData.activeUserName,
+    });
+    const roomLanguages = [
+      roomCreater.nativeLanguage,
+      roomCreater.learnLanguage,
+    ];
+    console.log('roomLanguages:', roomLanguages);
+
+    const created = await db.insertOne({
+      ...roomData,
+      roomLanguages,
+      messages: [],
+    });
     // console.log('created:', created);
   }
   return dbResponse;
